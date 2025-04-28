138d843ff324eff257394f697bf0bbaea7868d49
Kyosohee18 <hanashahky@gmail.com>
2025-04-28 11:06:27 +0700

    input data

--- class.cpp
+++ class.cpp
@@ -7,4 +7,15 @@ public:
     string Merkmobil, Tipemobil;
     private:
     int Hargamobil;
-
+    
+public:
+    void input_data(){
+        cout << "masukkan nama mobil   :";
+        cin >> Merkmobil;
+        cout << "masukkan tipe mobil   : ";
+        cin >> Tipemobil;
+        cout << "masukkan harga mobil  :";
+        cin >> Hargamobil;
+    
+    }
+    
